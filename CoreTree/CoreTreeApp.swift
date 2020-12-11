29f943ae79ecd29122c1b6aa392f760e5e9a9566
Ivan Dimitrov <biobeehive@abv.bg>
2020-12-11 17:39:07 +0200

    Initial Commit

---
+++ CoreTree/CoreTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreTreeApp.swift
+//  CoreTree
+//
+//  Created by Ivan Dimitrov on 11.12.20.
+//
+
+import SwiftUI
+
+@main
+struct CoreTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
